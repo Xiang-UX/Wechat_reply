06dd6e7890ba3de75cfe2a6c3013d8c97d9506c0
Xiang-UX <124876636+Xiang-UX@users.noreply.github.com>
2024-12-11 17:58:10 +0800

    Update Honor_of_Kings.py

--- 微信自动回复/Honor_of_Kings.py
+++ 微信自动回复/Honor_of_Kings.py
@@ -2,11 +2,10 @@ import requests,re
 
 
 def wz_hero(back_string):
-	# 请将 '元歌' 改为你想要查询的英��的名称
 	pattern = r'(wx|ios_wx|qq|ios_qq)(.*)'
 	match = re.search(pattern, back_string)
-	type = match.group(1)  # 区
-	hero = match.group(2)  # 英雄
+	type = match.group(1)
+	hero = match.group(2) 
 	url = f'https://api.pearktrue.cn/api/hero/?hero={hero}&type={type}'
 	response = requests.get(url)
 	data = response.json()
@@ -14,20 +13,20 @@ def wz_hero(back_string):
 		# Code = data["code"]
 		# Message = data["msg"]
 		data_data = data["data"]
-		UID = data_data["uid"]  # 英雄uid
-		Name = data_data["name"]  # 英雄名称
-		Alias = data_data["alias"]  # 英雄名称
-		platform = data_data["platform"]  # 区
-		Photo_URL = data_data["photo"]  # 头像照片
-		Province = data_data["province"]  # 金标位置
-		Province_Power = data_data["provincePower"]  # 金标分
-		city = data_data["city"]  # 银标位置
-		city_Power = data_data["cityPower"]  # 银标分
-		area = data_data["area"]  # 区标位置
-		area_Power = data_data["areaPower"]  # 铜标分
-		guobiao = data_data["guobiao"]  # 国标
-		# stamp = data_data["stamp"]  # 更新时间戳
-		update_Time = data_data["updatetime"]  # 更新时间
+		UID = data_data["uid"] 
+		Name = data_data["name"]
+		Alias = data_data["alias"]
+		platform = data_data["platform"]
+		Photo_URL = data_data["photo"]
+		Province = data_data["province"]
+		Province_Power = data_data["provincePower"]
+		city = data_data["city"]
+		city_Power = data_data["cityPower"]
+		area = data_data["area"]
+		area_Power = data_data["areaPower"]
+		guobiao = data_data["guobiao"]
+		# stamp = data_data["stamp"]
+		update_Time = data_data["updatetime"]
 		a = f"""
  英雄UID: {UID}
  英雄名称: {Name}
